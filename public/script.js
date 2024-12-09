22ab12bd8106f62dc15404638c97e1ab95ed18ee
BeLikeYEJOR <yejor599@gmail.com>
2024-12-08 22:40:44 -0800

    first commit

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const jokeContainer = document.querySelector("#joke");
+const btn = document.querySelector("#btn");
+const url =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&idRange=0-318";
+
+let getJoke = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+        jokeContainer.textContent = `${item.joke}`
+    });
+};
+
+btn.addEventListener('click', getJoke);
